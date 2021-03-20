731a2633831d737e707705f0c52acca55acc5b11
Zakaria Karlsen Tawfiq <zaktaw@gmail.com>
2021-03-20 10:42:36 +0100

    Now storing embed id in the embed.json file instead of in a global vriable in admin.js file

--- admin.js
+++ admin.js
@@ -1,8 +1,8 @@
 const Discord = require('discord.js');
+const fs = require('fs'); 
 const embedJson = require('./embed.json');
 
-let embedId;
-
+// updates the guest list with the new user list
 function updateEmbed(users, msg) {
     let usersString = String()
     if (users.length > 0) usersString += users[0].username
@@ -11,10 +11,12 @@ function updateEmbed(users, msg) {
     let updatedEmbed = makeEmbed()
     updatedEmbed.addField(`Guest list (${users.length})`, usersString.length > 0 ? usersString : "No one has joined the event yet")
 
-    msg.channel.messages.fetch(embedId)
+    msg.channel.messages.fetch(embedJson.id)
         .then(embed => embed.edit(updatedEmbed))
+        .catch(err => console.error(err))
 }
 
+// create  a new embed from the information provided in the embed.json file
 function makeEmbed() {
     let embed = new Discord.MessageEmbed()
 
@@ -30,11 +32,16 @@ function makeEmbed() {
    return embed;
 }
 
+// send the created embed to a Discord channel
 async function sendEmbed(msg) {
     let embed = makeEmbed();
     msg.channel.send(embed)
         .then(msg => {
-            embedId = msg.id
+            // update the embedID in the embed.json file with the ID of the sent embed
+            let embedID_json_file = embedJson;
+            embedID_json_file.id = msg.id;
+            embedID_json_file = JSON.stringify(embedID_json_file);
+            fs.writeFileSync('./embed.json', embedID_json_file);
         });
 }
 
--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const bot = new Discord.Client();
 const database = require('./database/database.js');
 const admin = require('./admin.js');
 
+const PREFIX = "ei "
+
 bot.on('ready', () => {
     console.log("Bot is online!");
 }); 
@@ -17,12 +19,12 @@ database.initDB()
 
 bot.on('message', (msg) => {
 
-    let args = msg.content.substring(process.env.EVENT_INVITER_BOT_PREFIX.length).split(" ");
+    let args = msg.content.substring(PREFIX.length).split(" ");
 
     // Prevent spam from bot
     if (msg.author.bot) return; // stops bot from replying to itself
     if (!msg.guild) return; // bot will only reply if message is sent in the guild (server)
-    if (!msg.content.startsWith(process.env.EVENT_INVITER_BOT_PREFIX)) return; // bot will only reply if the message starts with the specified prefix
+    if (!msg.content.startsWith(PREFIX)) return; // bot will only reply if the message starts with the specified prefix
 
     // Handle arguments given
     switch (args[0].toLowerCase()) {
